d0498becdace06509059b18e3f456fa8dfb98fd3
Chirag Jain <jainchg@gmail.com>
2019-11-25 02:57:21 +0530

    Preprocessing Training Data not working

--- Scripts/preProcess.py
+++ Scripts/preProcess.py
@@ -15,17 +15,49 @@ def validateXMLFiles():
     xmlschema.validate(article_training_data_loc, training_data_schema)
     xmlschema.validate(article_ground_truth_data_loc, ground_truth_schema)
 
+def cleantext(text):
+    '''Clean the text extracted from XML.'''
+    text = text.lower()
+    # text = text.replace("&amp;", "&")
+    # text = text.replace(bytes("&amp", 'utf-8'), bytes("&", 'utf-8'))
+    # text = text.replace("&gt;", ">")
+    # text = text.replace("&lt;", "<")
+    text = text.replace(bytes("<p>", 'utf-8'), bytes(" ", 'utf-8'))
+    # text = text.replace("</p>", " ")
+    text = text.replace(bytes("</p>", 'utf-8'), bytes(" ", 'utf-8'))
+    # text = text.replace(" _", " ")
+    text = text.replace(bytes(" _", 'utf-8'), bytes(" ", 'utf-8'))
+    # text = text.replace("–", "-")
+    text = text.replace(bytes("–", 'utf-8'), bytes("-", 'utf-8'))
+    # text = text.replace("”", "\"")
+    text = text.replace(bytes("”", 'utf-8'), bytes("\"", 'utf-8'))
+    # text = text.replace("“", "\"")
+    text = text.replace(bytes("“", 'utf-8'), bytes("\"", 'utf-8'))
+    # text = text.replace("’", "'")
+    text = text.replace(bytes("’", 'utf-8'), bytes("'", 'utf-8'))
+    text = text.replace(bytes("  ", 'utf-8'), bytes(" ", 'utf-8'))
+    return text
+
 def parseTrainingData():
     if is_article:
         tree = ET.parse(article_training_data_loc)
+        root = tree.getroot()
+        xmlstr = ET.tostring(root)
+        # print(xmlstr)
+        xmlstr = cleantext(xmlstr)
+        # xmlstr = xmlstr.lower()
+        print(xmlstr)
+        root = ET.fromstring(xmlstr)
     else:
         tree = ET.parse(publisher_training_data_loc)
-    root = tree.getroot()
-    print(root.tag)
-    print(root.attrib)
+    # root = tree.getroot()
+    # print(root.tag)
+    # print(root.attrib)
     for child in root:
         print(child.tag, child.attrib)
-        print(len(child))
+        for i in child:
+            print(i.text)
+        # print(len(child))
         break
 
 def getGroundTruth():
@@ -37,10 +69,12 @@ def getGroundTruth():
     is_hyper = {}
     for child in root:
         attrib = dict(child.attrib)
+        is_hyper[attrib['id']] = {}
         if attrib['hyperpartisan'] == 'true':
-            is_hyper[attrib['id']] = True
+            is_hyper[attrib['id']]['bias'] = True
         else:
-            is_hyper[attrib['id']] = False
+            is_hyper[attrib['id']]['bias'] = False
+        is_hyper[attrib['id']]['url'] = attrib['url']
     return is_hyper
 
 if __name__=='__main__':
