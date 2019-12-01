287d2858d21916b8ab341adee6a040edf50be47f
Chirag Jain <jainchg@gmail.com>
2019-12-02 00:19:28 +0530

    Encoding layers done

--- Scripts/createEmbeddings.py
+++ Scripts/createEmbeddings.py
@@ -11,8 +11,12 @@ from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
 batch_size = 50
 maxTokens = 200
 maxSentences = 200
-bert_model = 'bert-large-cased'
-bert_tokenizer = BertTokenizer.from_pretrained(bert_model)   
+
+bert_model_name = 'bert-base-cased'
+bert_tokenizer = None
+bert_model = None
+
+# model = None
 
 def setParser():
     parser = OptionParser()
@@ -28,15 +32,34 @@ def openTextConvert():
             arr = ts.split('\t')
             sent = arr[4]
             arr_sent = sent.split('<splt>')
+            # print(arr_sent)   
+            finalSent = '[CLS] '
             for i in range(len(arr_sent)):
-                arr_sent[i] = "[CLS] " + arr_sent[i] + " [SEP]"
+                finalSent += arr_sent[i].strip() + ' [SEP] '
+                # arr_sent[i] = "[CLS] " + arr_sent[i] + " [SEP]"
             # print(arr_sent[0])
-            arrTokenize(arr_sent)
+
+            tokenized, indexed_tokens, segment_ids = bertSentTokenize(finalSent)
+
+            indexed_tokens_tensor, segment_ids_tensor = get_tensors(indexed_tokens, segment_ids)
+            encoded_layers = getEncodingLayers(indexed_tokens_tensor, segment_ids_tensor)
+
             # TODO: Remove this break
             break
 
+def getEncodingLayers(tokens_tensor, segments_tensors):
+    # Predict hidden state features for each layer
+    global bert_model
+    with torch.no_grad():
+        encoded_layers, _ = bert_model(tokens_tensor, segments_tensors)      
+    return encoded_layers
+
+def extractEmbeddings():
+    pass
+
 def get_segments_ids(tokens):
     segments_ids = [1] * len(tokens)
+    print('seg id len', len(segments_ids))
     return segments_ids
 
 def get_tensors(indexed_tokens,segments_ids):
@@ -45,13 +68,23 @@ def get_tensors(indexed_tokens,segments_ids):
     
     return tokens_tensor, segments_tensors
 
-def arrTokenize(arr):
-    for i in arr:
-        tokens,indexed_tokens = sentTokenize(i)
-        segments_ids = get_segments_ids(tokens)
-        tokens_tensor,segments_tensors = get_tensors(indexed_tokens,segments_ids)
-        # TODO: Remove break
-        break
+def bertSentTokenize(sent):
+    global bert_tokenizer
+
+    # TODO: Change length here
+    tokenized = bert_tokenizer.tokenize(sent)[:512]
+    indexed_tokens = bert_tokenizer.convert_tokens_to_ids(tokenized)
+    segment_ids = get_segments_ids(tokenized)
+    
+    return tokenized, indexed_tokens, segment_ids
+    # print(segment_ids)
+    # print(tokenized)
+    # for i in arr:
+    #     tokens,indexed_tokens = sentTokenize(i)
+    #     segments_ids = get_segments_ids(tokens)
+    #     tokens_tensor,segments_tensors = get_tensors(indexed_tokens,segments_ids)
+    #     # TODO: Remove break
+    #     break
 
 def get_vectors(model,tokens_tensor,segments_tensors):
     # Convert the hidden state embeddings into single token vectors
@@ -84,31 +117,33 @@ def get_vectors(model,tokens_tensor,segments_tensors):
     print ("Number of layers per token:", len(token_embeddings[0])) 
     sentence_embedding = torch.mean(encoded_layers[11], 1)
 
-def sentTokenize(sent):
-    global bert_tokenizer
-    tokens = bert_tokenizer.tokenize(sent) 
-    #index of tokens in BERT
-    indexed_tokens = bert_tokenizer.convert_tokens_to_ids(tokens)
+# def sentTokenize(sent):
+#     global bert_tokenizer
+#     tokens = bert_tokenizer.tokenize(sent) 
+#     #index of tokens in BERT
+#     indexed_tokens = bert_tokenizer.convert_tokens_to_ids(tokens)
 
-    for tup in zip(tokens, indexed_tokens):
-        print (tup)    
-    print(tokens)
-    return tokens, indexed_tokens
+#     for tup in zip(tokens, indexed_tokens):
+#         print (tup)    
+#     print(tokens)
+#     return tokens, indexed_tokens
+
+def loadBertModel():
+    global bert_tokenizer, bert_model
+    bert_tokenizer = BertTokenizer.from_pretrained(bert_model_name, do_lower_case=False)   
+
+    bert_model = BertModel.from_pretrained(bert_model_name)
+    
+    # Put the model in "evaluation" mode, meaning feed-forward operation.
+    bert_model.eval()   
+    print('Bert loaded')
+    return None
 
 if __name__=='__main__':
-    options = setParser()
-    batch_size = options.bsize
-    maxTokens = options.maxtokens
-    maxSentences = options.maxsentences
+    # options = setParser()
+    # batch_size = options.bsize
+    # maxTokens = options.maxtokens
+    # maxSentences = options.maxsentences
+
+    loadBertModel()
     openTextConvert()
-
-
-    # Load pre-trained model (weights)
-    model = BertModel.from_pretrained(bert_model)
-
-    # Put the model in "evaluation" mode, meaning feed-forward operation.
-    model.eval()    
-
-
-
-    # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
